d68c7c3d31a1298c4c7ca02cdc5a449f80f5d33a
Anto-kumar <antokumar00@gmail.com>
2026-03-13 19:14:50 +0600

    next.config setting

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images:{
-     remotePatterns: [
+  serverExternalPackages: ["mongoose"],
+  images: {
+    remotePatterns: [
       { protocol: "https", hostname: "images.unsplash.com" },
+      { protocol: "https", hostname: "res.cloudinary.com" },
     ],
-  }
+  },
 };
 
 export default nextConfig;
